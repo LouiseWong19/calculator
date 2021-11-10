05446da62c4b7ddc27fc188377929ca2c9c017b1
Louise Wong <lw17122@bristol.ac.uk>
2021-11-10 20:06:04 +0000

    operators, added AC button

--- assets/styles/main.js
+++ assets/styles/main.js
@@ -7,34 +7,76 @@ const deciBtn = document.querySelector(".decimal__btn");
 const delBtn = document.querySelector("#del__btn");
 
 let firstValue = [];
-let secondValue = [];
+let savedValue = [];
+let operator;
+let answer;
 
 
-
+// Number Buttons //
 numBtn.forEach((numberbtn) => {
-  numberbtn.addEventListener("click", () =>{
+  numberbtn.addEventListener("click", (event) =>{
     firstValue.push(numberbtn.value);
     displayInput.innerHTML = firstValue.join("");
-    console.log("number");
+    console.log(firstValue);
   })
   });
 
-delBtn.addEventListener("click",() =>{
+// Delete Button //
+delBtn.addEventListener("click",(event) =>{
   firstValue.pop()
   displayInput.innerHTML=firstValue.join("");
-  console.log("delete");
+  console.log(firstValue);
 })
 
-// const multiply = (firstValue,secondValue)
+// Operator Buttons//
+operBtn.forEach((operatorBtn) => {
+  operatorBtn.addEventListener("click", (event) =>{
+    savedValue = parseInt(firstValue.join(""));
+     firstValue = [];
+     operator = operatorBtn.value;
+     displayInput.innerHTML += operatorBtn.innerText; //need to fix restricting only one oper
+  })
+})
 
-// operBtn.forEach((operatorBtn) => {
-//   operatorBtn.addEventListener("click", () =>{
-//     if 
-//   })
-// })
 
-  // operBtn.forEach((operatorbtn) => {
-  //   operatorbtn.addEventListener("click",() =>{
-  //     if 
-  //   })
-  // });
+// Equal Button // 
+
+const result = () =>{
+  if (operator === "*"){
+    answer = parseInt(firstValue.join("")) * savedValue;
+    displayOutput.innerHTML = answer;
+  }else if (operator === "/"){
+    answer = parseInt(firstValue.join("")) / savedValue;
+    displayOutput.innerHTML = answer;
+  }else if (operator === "+"){
+    answer = parseInt(firstValue.join("")) + savedValue;
+    displayOutput.innerHTML = answer;
+  }else if (operator === "-"){
+    answer = parseInt(firstValue.join("")) - savedValue;
+    displayOutput.innerHTML = answer;
+  }
+}
+
+equalBtn.addEventListener("click",result)
+
+
+// const equal = (firstValue, savedValue, answer, operatorBtn) =>{
+//   currentValue = parseInt(firstValue.join(""));
+//   switch (operatorBtn){
+//     case "+":
+//       answer = currentValue + savedValue;
+//       break;
+//     case "-":
+//     answer = currentValue - savedValue;
+//     break;
+//     case "×":
+//     answer = currentValue * savedValue;
+//     break;
+//     case "÷":
+//     answer = currentValue % savedValue;
+//     break;
+//   }
+// }
+
+//  equalBtn.addEventListener("click", equal);
+
