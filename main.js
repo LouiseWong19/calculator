b58710325908c8d43b2cbc024bb2435abd0b088d
Louise Wong <lw17122@bristol.ac.uk>
2021-11-25 16:17:27 +0000

    changed class names

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const displayInput = document.querySelector(".calculator__input");
 const displayOutput = document.querySelector(".calculator__output");
-const operBtn = document.querySelectorAll(".operator__btn");
-const numBtn = document.querySelectorAll(".number__btn");
-const equalBtn = document.querySelector(".equal__btn");
+const operBtn = document.querySelectorAll(".calculator__operator");
+const numBtn = document.querySelectorAll(".calculator__number");
+const equalBtn = document.querySelector(".calculator__equal");
 //const deciBtn = document.querySelector(".decimal__btn");
-const delBtn = document.querySelector("#del__btn");
-const acBtn = document.querySelector("#delAll__btn");
+const delBtn = document.querySelector(".calculator__del");
+const acBtn = document.querySelector(".calculator__del-all");
 
 let firstValue = [];
 let savedValue = [];
@@ -14,9 +14,9 @@ let answer;
 
 
 // Number Buttons //
-numBtn.forEach((numberbtn) => {
-  numberbtn.addEventListener("click", () =>{
-    firstValue.push(numberbtn.value);
+numBtn.forEach((numberBtn) => {
+  numberBtn.addEventListener("click", () =>{
+    firstValue.push(numberBtn.value);
     displayInput.innerHTML = firstValue.join("");
     console.log(firstValue);
   })
@@ -38,33 +38,33 @@ acBtn.addEventListener("click",() =>{
   console.clear()
 })
 
-// Operator Buttons//
+// Operator Buttons //
 operBtn.forEach((operatorBtn) => {
   operatorBtn.addEventListener("click", () =>{
     savedValue = parseFloat(firstValue.join(""));
      firstValue = [];
      operator = operatorBtn.value;
-     displayInput.innerHTML += operatorBtn.innerText; 
+     displayInput.innerHTML += operatorBtn.innerText;
+     
   })
 })
 
 
 // Equal Button // 
-const result = () =>{
-    if (operator === "*"){
-      answer = parseFloat(firstValue.join("")).toFixed(5) * savedValue.toFixed(5);
-      displayOutput.innerHTML = answer;
-    }else if (operator === "/"){
-      answer =  savedValue.toFixed(5) / parseFloat(firstValue.join("")).toFixed(5);
-      displayOutput.innerHTML = answer;
-    }else if (operator === "+"){
-      answer = parseFloat(firstValue.join("")) + savedValue;
-      displayOutput.innerHTML = answer.toFixed(3);
-    }else if (operator === "-"){
-      answer = savedValue - parseFloat(firstValue.join(""));
-      displayOutput.innerHTML = answer.toFixed(3);
-    }
+const result = () => {
+  displayInput.innerHTML=firstValue.join("");
+  if (operator === "*") {
+    answer = parseFloat(firstValue.join("")).toFixed(5) * savedValue.toFixed(5);
+  } else if (operator === "/") {
+    answer = savedValue.toFixed(5) / parseFloat(firstValue.join("")).toFixed(5);
+  } else if (operator === "+") {
+    answer = (parseFloat(firstValue.join("")) + savedValue).toFixed(3);
+  } else if (operator === "-") {
+    answer = (savedValue - parseFloat(firstValue.join(""))).toFixed(3);
   }
+  displayOutput.innerHTML = answer;
+
+};
 
 equalBtn.addEventListener("click",result)
 
